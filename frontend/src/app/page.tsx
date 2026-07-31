dda9badf063157f0dd43ac47dd8865edd7031afb
daireks <derek.g.thompson@icloud.com>
2026-07-31 16:58:18 -0400

    Completed basic playlist layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,25 +4,87 @@ export default function Home() {
   return (
     <div className="bg-slate-700 w-screen h-screen flex justify-center items-center">
       <div className="bg-slate-200 h-[60vh] aspect-video flex-row">
-        <div className="bg-slate-300 w-full h-[20%] flex-row">
+        <div className="bg-slate-300 w-full h-[15%] flex-row">
           <div className="bg-slate-400 w-full h-[50%] flex">
             <div className="outline-1 outline-slate-600 m-1 flex-1 bg-slate-500">
 
             </div>
-            <div className="outline-1 outline-slate-600 m-1 flex-1 bg-slate-500">
+            <div className="outline-1 outline-slate-600 m-1 flex-4 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-3 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-4 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-2 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-2 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-4 bg-slate-500">
 
             </div>
           </div>
           <div className="bg-slate-400 w-full h-[50%] flex">
-            <div className="outline-1 outline-slate-600 m-1 flex-1 bg-slate-500">
+            <div className="outline-1 outline-slate-600 m-1 flex-7 bg-slate-500">
 
             </div>
-            <div className="outline-1 outline-slate-600 m-1 flex-1 bg-slate-500">
+            <div className="outline-1 outline-slate-600 m-1 flex-1 aspect-square bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-1 aspect-square bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-4 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-7 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-7 bg-slate-500">
+
+            </div>
+            <div className="outline-1 outline-slate-600 m-1 flex-3 bg-slate-500">
 
             </div>
           </div>
         </div>
-        <div className="bg-slate-300 w-full h-[80%] flex">
+        <div className="bg-slate-600 w-full h-[3%] flex p-0.5 space-x-1">
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-600 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+          <div className="bg-slate-600 aspect-square"/>
+          <div className="bg-slate-500 aspect-square"/>
+        </div>
+        <div className="bg-slate-600 w-full h-[7%] flex">
+          <div className="bg-slate-600 w-[12%] flex">
+
+          </div>
+          <div className="bg-slate-700 w-[10%] flex">
+
+          </div>
+          <div className="bg-slate-800 w-[78%] flex-row">
+            <div className="bg-slate-500 h-[50%]">
+
+            </div>
+            <div className="bg-slate-800 h-[50%]">
+
+            </div>
+          </div>
+        </div>
+        <div className="bg-slate-300 w-full h-[75%] flex">
           <div className="bg-slate-500 w-[10%] flex">
 
           </div>
