3a109f4dfb9ca1b636ebd5a992edac78554f390b
daireks <derek.g.thompson@icloud.com>
2026-08-25 21:41:43 -0400

    Made colors dynamically interchangeable + added shadcn slider component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,31 +1,78 @@
 import Image from "next/image";
 import Mask from "../../components/Mask";
 import Color from "../../components/Color";
+import { colorModels } from "../../lib/colors";
 
-const bgColorLightest = "#5A7474";
-const bgColorLight = "#576268";
-const bgColor = "#414C52";
-const bgColorDark = "#3C464C";
-const bgColorDarkest = "#2E383E"
+const hue = 180
+const saturation = 0
+const brightness = 0
+
+//Calculated
+const bgColorLightest = `hsl(
+  ${198 + hue},
+  ${colorModels.bgcolorLightest.saturation(saturation, brightness)}%,
+  ${colorModels.bgcolorLightest.brightness(brightness)}%
+)`;
+const bgColorLight = `hsl(
+  ${198 + hue},
+  ${colorModels.bgcolorLight.saturation(saturation, brightness)}%,
+  ${colorModels.bgcolorLight.brightness(brightness)}%
+)`;
+const bgColor = `hsl(
+  ${198 + hue},
+  ${colorModels.bgColor.saturation(saturation, brightness)}%,
+  ${colorModels.bgColor.brightness(brightness)}%
+)`;
+const bgColorDark = `hsl(
+  ${198 + hue},
+  ${colorModels.bgcolorDark.saturation(saturation, brightness)}%,
+  ${colorModels.bgcolorDark.brightness(brightness)}%
+)`;
+const bgColorDarkest = `hsl(
+  ${198 + hue},
+  ${colorModels.bgcolorDarkest.saturation(saturation, brightness)}%,
+  ${colorModels.bgcolorDarkest.brightness(brightness)}%
+)`;
+
+const measureColor1 = `hsl(
+  ${198 + hue},
+  ${colorModels.measureColor1.saturation(saturation, brightness)}%,
+  ${colorModels.measureColor1.brightness(brightness)}%
+)`;
+const measureColor2 = `hsl(
+  ${198 + hue},
+  ${colorModels.measureColor2.saturation(saturation, brightness)}%,
+  ${colorModels.measureColor2.brightness(brightness)}%
+)`;
+const measureCountColor = `hsl(
+  ${198 + hue},
+  ${colorModels.bgcolorDarkest.saturation(saturation, brightness)}%,
+  ${colorModels.bgcolorDarkest.brightness(brightness)}%
+)`;
+
+const patternListColor = `hsl(
+  ${198 + hue},
+  ${colorModels.patternListColor.saturation(saturation, brightness)}%,
+  ${colorModels.patternListColor.brightness(brightness)}%
+)`;
+const trackListColor = `hsl(
+  ${198 + hue},
+  ${colorModels.trackListColor.saturation(saturation, brightness)}%,
+  ${colorModels.trackListColor.brightness(brightness)}%
+)`;
+
+//Not Calculated
+const patternHeadColor = "#434F55";
+const patternBodyColor = measureCountColor;
 
 const accent1 = "#7DDB00";
 const accent2 = "#FF9F41";
 
 const tempoColor = "#BBBBBB";
 
-const measureColor1 = "#30454F";
-const measureColor2 = "#293F49";
-const measureCountColor = "#1B2932"
-
-const patternListColor = "#2E3C46";
-const trackListColor = "#475258";
-
-const patternHeadColor = "#434F55";
-const patternBodyColor = "#25363E";
-
 export default function Home() {
   return (
-    <div className="bg-slate-700 w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center" style={{ backgroundColor: `hsl(${hue+198}, ${((saturation+256)/512)*100}%, ${((brightness+256)/512)*100}%)` }}>
       <div className="bg-slate-600 h-[60vh] aspect-video flex-row">
         <div className="bg-slate-600 w-full h-[15%] flex-row">
           <div className="w-full h-[50%] flex" style={{ backgroundColor: bgColor }}>
@@ -173,6 +220,10 @@ export default function Home() {
             <Mask src="/images/ui_masks/PatternHeaderMask.png" color={patternHeadColor} className="object-contain scale-100 scale-x-[1.015]"/>
           </div>
         </div>
+      </div>
+
+      <div className="bg-slate-500 aspect-video h-[30vh]">
+        
       </div>
     </div>
   );
